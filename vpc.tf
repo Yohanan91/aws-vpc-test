0b4b5052404901d1afd6f30198a399253c5491be
Yohanan Oloke-Ehisuan <yohanan@Yohanans-MacBook-Pro.local>
2026-02-21 00:10:29 -0500

    trying after publishing

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 module "network" {
-  source               = "git@github.com:Yohanan91/terraform-aws-vpc.git"
+  source               = "Yohanan91/vpc/aws"
   cidr                 = "10.0.0.0/16"
   public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnet_cidrs = ["10.0.10.0/24", "10.0.11.0/24"]
